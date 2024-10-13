13760ddd09d824f1742c9e64b167fde7ba099445
jh.jun <dnova13@gmail.com>
2024-10-13 16:46:00 +0900

    05. JDBC 이해 - JDBC 개발: db 조회, 수정, 삭제

--- 4_spring_db_p1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ 4_spring_db_p1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,12 +1,18 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -14,7 +20,26 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
 
-        Member member = new Member("memberV0", 10000);
+        Random random = new Random();
+        int randomNumber = random.nextInt(1000) + 1; // 1부터 1000까지의 랜덤 숫자
+
+        // save
+        Member member = new Member("memberV" + randomNumber, 10000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+
+        // update : money 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+
+        // delete
+        repository.delete(member.getMemberId());
     }
 }
