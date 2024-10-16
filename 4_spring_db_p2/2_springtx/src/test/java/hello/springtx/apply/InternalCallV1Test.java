3a9211399febce831bbc2d185e5152c222e1f582
jh.jun <dnova13@gmail.com>
2024-10-16 10:29:02 +0900

    20. 스프링 트랜잭션 이해 - AOP 주의 사항

---
+++ 4_spring_db_p2/2_springtx/src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,65 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    static class CallService {
+
+        public void external() { // 해당 메소드 자체에는 트랜잭션이 붙어있지않음
+            log.info("call external");
+            printTxInfo();
+            internal(); // 이 메소드는 트랜잭션이 붙어있으므로 , 트랜잭션이 되길 예상하고 사용
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active ={}", txActive);
+//            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+//            log.info("tx readOnly ={}", readOnly);
+        }
+    }
+
+}
